7b3b935bd8e61f6159204035b39ed77205b76b17
Amos <amosdorceus2010@gmail.com>
2022-11-16 18:53:48 +0100

    tryfix:token

--- backend/src/models.py
+++ backend/src/models.py
@@ -68,13 +68,74 @@ class Song(SQLModel, table=True):
 class User(SQLModel, table=True):
     id: Optional[int] = Field(default=None, primary_key=True)
     username: str
+    firstname: str
+    lastname: str
+    telephone: str
     email: str
     password: str = "password"
     is_admin: Boolean
+    user_payment: List["UserPayment"] = Relationship(back_populates="user")
 
+    def get_list_album(item):
+        data = dict(item)
+        data["list_album"]= item.albums
+        return data
+
+
+class ShoppingSession(SQLModel, table=True):
+    id: Optional[int] = Field(default=None, primary_key=True)
+    total: float
+    user_id: Optional[int] = Field(default=None, foreign_key="user.id")
+
+class CartItem(SQLModel, table=True):
+    id: Optional[int] = Field(default=None, primary_key=True)
+    total: float
+    user_id: Optional[int] = Field(default=None, foreign_key="user.id")
+    album_id: Optional[int] = Field(default=None, foreign_key="album.id")
+    qty: int
+    shopping_session_id: Optional[int] = Field(default=None, foreign_key="shoppingsession.id")
+
+class UserPayment(SQLModel, table=True):
+    id: Optional[int] = Field(default=None, primary_key=True)
+    payment_type: str
+    provider: str
+    user_id: Optional[int] = Field(default=None, foreign_key="user.id")
+    user: Optional[User] = Relationship(back_populates="user_payment")
+
+    
+
+class UserAddress(SQLModel, table=True):
+    id: Optional[int] = Field(default=None, primary_key=True)
+    address_line1: str
+    address_line2: str
+    city: str
+    postal_code: str
+    country: str
+    mobile: str
+    user_id: Optional[int] = Field(default=None, foreign_key="user.id")
+
+class OrderDetail(SQLModel, table=True):
+    id: Optional[int] = Field(default=None, primary_key=True)
+    total: str
+    user_id: Optional[int] = Field(default=None, foreign_key="user.id")
+
+class OrderItem(SQLModel, table=True):
+    id: Optional[int] = Field(default=None, primary_key=True)
+    qty: int
+    order_detail_id: Optional[int] = Field(default=None, foreign_key="orderdetail.id")
+    album_id: Optional[int] = Field(default=None, foreign_key="album.id")
+class PaymentDetail(SQLModel, table=True):
+    id: Optional[int] = Field(default=None, primary_key=True)
+    amount: float
+    status: Boolean
+    order_detail_id: Optional[int] = Field(default=None, foreign_key="orderdetail.id")
+
+    
+    
 class Login():
     username: str
     password: str
+
 
 
 #Create 
