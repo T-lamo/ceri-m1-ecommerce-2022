74ffc41092e78ef4a2af8e0bf74cfff5b10fd9c2
Amos <amosdorceus2010@gmail.com>
2022-11-23 23:22:21 +0100

    add other table models, interfaces, cruds

--- frontend/src/stores/index.ts
+++ frontend/src/stores/index.ts
@@ -1,12 +1,18 @@
 import { defineStore } from "pinia";
-import type { Album, Artist, Category, Promo, Song } from "@/models";
-import {
-  read_albums,
-  read_artists,
-  read_categories,
-  read_promos,
-  read_songs,
-} from "@/services/crud";
+import type {
+  Album,
+  Artist,
+  CartItem,
+  Category,
+  OrderDetail,
+  OrderItem,
+  PaymentDetail,
+  Promo,
+  ShoppingSession,
+  Song,
+  User,
+} from "@/models";
+
 import { ref } from "vue";
 
 export const useAppStore = defineStore("app", () => {
@@ -15,11 +21,30 @@ export const useAppStore = defineStore("app", () => {
   const list_album = ref<Album[]>([]);
 
   const list_song = ref<Song[]>([]);
+  const list_cart_item = ref<CartItem[]>([]);
+  const list_order_detail = ref<OrderDetail[]>([]);
+  const list_payment_detail = ref<PaymentDetail[]>([]);
+  const list_shopping_Session = ref<ShoppingSession[]>([]);
+  const list_order_item = ref<OrderItem[]>([]);
+  const list_user = ref<User[]>([]);
+  const list_user_address = ref<CartItem[]>([]);
 
   const list_promo = ref<Promo[]>([]);
 
   const list_category = ref<Category[]>([]);
 
-
-  return { list_artist, list_album, list_song, list_promo, list_category };
+  return {
+    list_artist,
+    list_album,
+    list_song,
+    list_promo,
+    list_category,
+    list_cart_item,
+    list_order_detail,
+    list_payment_detail,
+    list_shopping_Session,
+    list_order_item,
+    list_user,
+    list_user_address,
+  };
 });
