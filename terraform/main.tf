ec1d95e8577a50826ecc991ab269546771abe25e
Félix Voituret <Faylixe@users.noreply.github.com>
2022-12-12 14:07:14 +0100

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -8,58 +8,53 @@ terraform {
   }
 }
 
-
 provider "google" {
   project = "ceri-m1-ecommerce-2022"
   region  = "europe-west1"
 }
 
-data "google-secret-manager-secret" "address" {
+data "google_secret_manager_secret" "address" {
   secret_id = "mysql-address"
 }
 
-data "google-secret-manager-secret" "user" {
+data "google_secret_manager_secret" "user" {
   secret_id = "mysql-database-greenfish"
 }
 
-data "google-secret-manager-secret" "password" {
+data "google_secret_manager_secret" "password" {
   secret_id = "mysql-user-greenfish"
 }
 
-
-
-resource "google-cloud" "backend" {
-  template {
-    spec {
-      container {
-        //...
-        env {
-          name = "DATABASE_ADDRESS"
-          value_from {
-            secret-key-ref {
-              name = data.google.secret.address.mysql_database
-            }
-          }
-        }
-        env {
-          name = "DATABASE_USER"
-          value_from {
-            secret-key-ref {
-              name = data.google.secret.user.secret_id
-            }
-          }
-        }
-
-        env {
-          name = "DATABASE_PASSWORD"
-          value_from {
-            secret-key-ref {
-              name = data.google.secret.password.secret_id
-            }
-          }
-        }
-      }
-    }
-  }
-}
-
+# resource "google-cloud" "backend" {
+#  template {
+#    spec {
+#      container {
+#        //...
+#        env {
+#          name = "DATABASE_ADDRESS"
+#          value_from {
+#            secret-key-ref {
+#              name = data.google.secret.address.mysql_database
+#            }
+#          }
+#        }
+#        env {
+#          name = "DATABASE_USER"
+#          value_from {
+#            secret-key-ref {
+#              name = data.google.secret.user.secret_id
+#            }
+#          }
+#        }
+#        env {
+#          name = "DATABASE_PASSWORD"
+#          value_from {
+#            secret-key-ref {
+#              name = data.google.secret.password.secret_id
+#            }
+#          }
+#        }
+#      }
+#    }
+#  }
+#}
