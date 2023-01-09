503049786b7939fd8c791c8e8c8eb662f69dbe43
Amos <amosdorceus2010@gmail.com>
2023-01-09 10:16:31 +0100

    add output

--- terraform/main.tf
+++ terraform/main.tf
@@ -103,5 +103,13 @@ resource "google_cloud_run_service" "frontend" {
   }
 }
 
+output "back_url" {
+  value = google_cloud_run_service.backend.status[0].url
+}
+
+output "front_url" {
+  value = google_cloud_run_service.frontend.status[0].url
+}
+
 
 
