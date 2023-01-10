043fefa0aa759f34a6dd680680869b95a430a086
Amos <amosdorceus2010@gmail.com>
2023-01-10 10:18:45 +0100

    new release

--- frontend/src/plugins/webfontloader.ts
+++ frontend/src/plugins/webfontloader.ts
@@ -4,13 +4,17 @@
  * webfontloader documentation: https://github.com/typekit/webfontloader
  */
 
-export async function loadFonts () {
-  const webFontLoader = await import(/* webpackChunkName: "webfontloader" */'webfontloader')
+export async function loadFonts() {
+  const webFontLoader = await import(
+    /* webpackChunkName: "webfontloader" */ "webfontloader"
+  );
 
   webFontLoader.load({
     google: {
       // families: ['Roboto:100,300,400,500,700,900&display=swap'],
-      families: ['Poppins:wght@300;600;700&800&family=Rubik:wght@300;400;500&display=swap']
+      families: [
+        "Poppins:wght@300;600;700&800&family=Rubik:wght@300;400;500&display=swap",
+      ],
     },
-  })
+  });
 }
--- terraform/main.tf
+++ terraform/main.tf
@@ -26,29 +26,34 @@ data "google_secret_manager_secret" "password" {
   secret_id = "mysql-user-greenfish"
 }
 
+data "google_secret_manager_secret" "algolia_app_id" {
+  secret_id = "algolia-greenfish-application-id"
+}
 
-
+data "google_secret_manager_secret" "algolia_app_key" {
+  secret_id = "algolia-greenfish-api-key"
+}
 resource "google_cloud_run_service" "backend" {
-  name="greenfish-backend"
+  name     = "greenfish-backend"
   location = "europe-west1"
 
   template {
-     metadata {
-        annotations = {
-          "autoscaling.knative.dev/maxScale" = "1"
-        }
+    metadata {
+      annotations = {
+        "autoscaling.knative.dev/maxScale" = "1"
       }
+    }
     spec {
       service_account_name = "terraform-greenfish@ceri-m1-ecommerce-2022.iam.gserviceaccount.com"
-     
+
       containers {
-        image = "europe-west1-docker.pkg.dev/ceri-m1-ecommerce-2022/greenfish/backend:0.0.1"
+        image = "europe-west1-docker.pkg.dev/ceri-m1-ecommerce-2022/greenfish/backend:1.0.0"
         env {
           name = "DATABASE_ADDRESS"
           value_from {
             secret_key_ref {
               name = data.google_secret_manager_secret.address.secret_id
-              key = "latest"
+              key  = "latest"
 
             }
           }
@@ -58,7 +63,7 @@ resource "google_cloud_run_service" "backend" {
           value_from {
             secret_key_ref {
               name = data.google_secret_manager_secret.user.secret_id
-              key = "latest"
+              key  = "latest"
             }
           }
         }
@@ -68,11 +73,35 @@ resource "google_cloud_run_service" "backend" {
           value_from {
             secret_key_ref {
               name = data.google_secret_manager_secret.password.secret_id
-              key = "latest"
+              key  = "latest"
 
             }
           }
         }
+
+        env {
+          name = "ALGOLIA_KEY"
+          value_from {
+            secret_key_ref {
+              name = data.google_secret_manager_secret.algolia_app_key.secret_id
+              key  = "latest"
+
+            }
+          }
+        }
+
+
+        env {
+          name = "ALGOLIA_APP_ID"
+          value_from {
+            secret_key_ref {
+              name = data.google_secret_manager_secret.algolia_app_id.secret_id
+              key  = "latest"
+
+            }
+          }
+        }
+
       }
     }
   }
@@ -80,20 +109,20 @@ resource "google_cloud_run_service" "backend" {
 
 
 resource "google_cloud_run_service" "frontend" {
-  name="greenfish-frontend"
+  name     = "greenfish-frontend"
   location = "europe-west1"
   template {
 
-     metadata {
-        annotations = {
-          "autoscaling.knative.dev/maxScale" = "1"
-        }
+    metadata {
+      annotations = {
+        "autoscaling.knative.dev/maxScale" = "1"
       }
+    }
     spec {
       service_account_name = "terraform-greenfish@ceri-m1-ecommerce-2022.iam.gserviceaccount.com"
-     
+
       containers {
-        image = "europe-west1-docker.pkg.dev/ceri-m1-ecommerce-2022/greenfish/frontend:0.0.1"
+        image = "europe-west1-docker.pkg.dev/ceri-m1-ecommerce-2022/greenfish/frontend:1.0.0"
         env {
           name  = "back_url"
           value = google_cloud_run_service.backend.status.0.url
@@ -106,16 +135,16 @@ resource "google_cloud_run_service" "frontend" {
 
 resource "google_cloud_run_service_iam_member" "backend" {
   location = google_cloud_run_service.backend.location
-  service = google_cloud_run_service.backend.name
-  role    = "roles/run.invoker"
-  member  = "allUsers"
+  service  = google_cloud_run_service.backend.name
+  role     = "roles/run.invoker"
+  member   = "allUsers"
 }
 
 resource "google_cloud_run_service_iam_member" "frontend" {
   location = google_cloud_run_service.frontend.location
-  service = google_cloud_run_service.frontend.name
-  role    = "roles/run.invoker"
-  member  = "allUsers"
+  service  = google_cloud_run_service.frontend.name
+  role     = "roles/run.invoker"
+  member   = "allUsers"
 }
 output "back_url" {
   value = google_cloud_run_service.backend.status[0].url
